ebbbeec78bf6235c9307569b57039d6a0f01f7c7
Your Name <xubing>
2020-04-25 18:04:20 +0800

    frist

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * File AuthController.php
+ *
+ * @author Tuan Duong <bacduong@gmail.com>
+ * @package Laravue
+ * @version 1.0
+ */
+namespace App\Http\Controllers;
+
+use App\Laravue\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\UserResource;
+
+/**
+ * Class AuthController
+ *
+ * @package App\Http\Controllers
+ */
+class AuthController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (!Auth::attempt($credentials)) {
+            return response()->json(new JsonResponse([], 'login_error'), Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('laravue');
+
+        return response()->json(new UserResource($user), Response::HTTP_OK)->header('Authorization', $token->plainTextToken);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json((new JsonResponse())->success([]), Response::HTTP_OK);
+    }
+
+    public function user()
+    {
+        return new UserResource(Auth::user());
+    }
+}
